0316af74919e121bf56980f9e71605d07829f7e1
herothezero90 <94224236+herothezero90@users.noreply.github.com>
2024-01-22 19:03:23 +0100

    added header image

--- script.js
+++ script.js
@@ -71,6 +71,7 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
+// eslint-disable-next-line no-unused-vars
 function topFunction() {
 	document.body.scrollTop = 0; // For Safari
 	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
